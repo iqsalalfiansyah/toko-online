7224f4d8ba53b3ab113cb0ca7d4e1ca16ab0d005
iqsalalfiansyah <iqsalalfiansyah@gmail.com>
2022-08-21 12:37:27 +0700

    toko-online

---
+++ footer.php
@@ -0,0 +1,32 @@
+<!-- <div class="asdsa"style="margin-top: 39px;"> -->
+
+	<div class="footer">
+		<div class="container fluid my-5">
+			<div class="footer-grids">
+				<div class="col-sm-6 footer-grid animated wow slideInLeft" data-wow-delay=".5s">
+					<h3>About Us</h3>
+					<p>Toko Jaks_Store ini adalah suatu toko yang menjual berbagai macam alat alat outdoot<span>Toko ini buka setiap hari dari jam 09.00 - 22.00 WIB</span></p>
+				</div>
+				<div class="col-sm-6 footer-grid animated wow slideInLeft" data-wow-delay=".6s">
+					<h3>Contact Info</h3>
+					<ul>
+						<li><i class="glyphicon glyphicon-map-marker" aria-hidden="true"></i><a href="https://goo.gl/maps/vHu9CowdsxXtXP7z6">Jl. Wijaya 1,Kampung Deret <span>RT 11/05 No. 29</span> <span> Kel. Petogogan, Kec. Kebayoran Baru, Jakarta Selatan 12170</span></a></li>
+						<li><i class="glyphicon glyphicon-envelope" aria-hidden="true"></i><a href="mailto:info@example.com">Jakstore@gmail.com</a></li>
+						<li><i class="glyphicon glyphicon-earphone" aria-hidden="true"></i><a href="https://api.whatsapp.com/send?phone=+62 857-7153-5764&text=Hallo Admin Saya ingin memesan alat outdoor">+62 857-7153-5764</a></li>
+					</ul>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="footer-logo animated wow slideInUp" data-wow-delay=".5s">
+				<h2><a href="index.php">Jaks_Store <span>Toko alat outdoor</span></a></h2>
+			</div>
+			<div class="copy-right animated wow slideInUp" data-wow-delay=".5s">
+				<p>&copy 2022 Jaks_Store. All rights reserved</p>
+			</div>
+		</div>
+	</div>
+<!-- //footer -->
+<!-- </div> -->
+</body>
+
+</html>
